b9654591d986cc6310d8d805a9abd9a99b14c524
mahsasadeghi <mahsa.sadeghi@berkeley.edu>
2020-10-23 12:50:30 -0700

    added lab8,lab12,lab13, and lab14 via upload

---
+++ lab12/YourSort.java
@@ -0,0 +1,20 @@
+/* YourSort.java */
+import java.util.Arrays.*;
+public class YourSort {
+
+  public static void sort(int[] A) {
+      java.util.Arrays.sort(A);
+      // int[] counts = new int[A.length];
+      // for(int i=0; i<A.length; i++){
+          // counts[A[i]]++;
+          // }
+          // int z = 0;
+      // for(int k=0; k<counts.length; k++){
+          // while(counts[k] > 0){
+          // A[z] = k;
+          // z++;
+          // counts[k]--;
+      // }
+      //}
+  }
+}
